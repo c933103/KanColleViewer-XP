3ab1053f2fe681057252bec4221f00defebe1601
Linnaea Von Lavia <le.concorde.4590@gmail.com>
2015-02-09 16:23:57 +0800

    Cherry pick commit: 艦隊タブを新しくしてみるテスト
    Conflicts:
    	Grabacr07.KanColleWrapper/Models/ShipInfo.cs

--- Grabacr07.KanColleWrapper/Models/ShipInfo.cs
+++ Grabacr07.KanColleWrapper/Models/ShipInfo.cs
@@ -44,6 +44,8 @@ namespace Grabacr07.KanColleWrapper.Models
 			get { return shipType ?? (shipType = KanColleClient.Current.Master.ShipTypes[this.RawData.api_stype]) ?? ShipType.Dummy; }
 		}
 
+        public int[] Slots { get { return this.RawData.api_maxeq; } }
+
 		#region 用意したけど使わないっぽい？
 
 		/// <summary>
