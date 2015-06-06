30df1648dd822d29bab6ccd6a84c7d08be93ff12
Linnaea Von Lavia <linnaea-von-lavia@live.com>
2015-06-06 22:13:58 +0800

    increment build number

--- Plugins/DataLogger/LynLoggerMain.cs
+++ Plugins/DataLogger/LynLoggerMain.cs
@@ -25,7 +25,7 @@ namespace LynLogger
         {
             get
             {
-                return "3.8.2-1.1(T5)"
+                return "3.8.2-1.1(T6)"
 #if DEBUG
                      + "d"
 #endif
