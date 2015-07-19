c933103/KanColleViewer

performance tweaks

Grabacr07.KanColleWrapper/Dockyard.cs
@@ -70,20 +70,9 @@ namespace Grabacr07.KanColleWrapper
 
 		internal void Update(kcsapi_kdock[] source)
 		{
-			if (this.Docks.Count == source.Length)
-			{
-				foreach (var raw in source)
-				{
-					var target = this.Docks[raw.api_id];
-					if (target != null) target.Update(raw);
-				}
-			}
-			else
-			{
-				this.Docks.ForEach(x => x.Value.Dispose());
-				this.Docks = new MemberTable<BuildingDock>(source.Select(x => new BuildingDock(x)));
-			}
-		}
+            if(this.Docks.SetValueRange(source, x => x.api_id, x => new BuildingDock(x), (obj, dat) => obj.Update(dat), true, x => x.Dispose()))
+                RaisePropertyChanged(nameof(Docks));
+        }
 
 		private void GetShip(kcsapi_kdock_getship source)
 		{

Grabacr07.KanColleWrapper/Itemyard.cs
@@ -14,17 +14,11 @@ namespace Grabacr07.KanColleWrapper
 	public class Itemyard : NotificationObject
 	{
 		/// <summary>
-		/// 出撃中にドロップで入手した装備の数。
-		/// 帰投して slot_item を取得するまでは新しい装備の ID が判らないので、数だけ控えておき、SlotItemsCount で使用する。
-		/// </summary>
-		private int droppedItemsCount;
-
-		/// <summary>
 		/// <see cref="SlotItems"/> と、出撃中に入手したものを含んだ装備数を取得します。
 		/// </summary>
 		public int SlotItemsCount
 		{
-			get { return this.SlotItems.Count + droppedItemsCount; }
+			get { return this.SlotItems.Count; }
 		}
 
 		#region SlotItems 変更通知プロパティ
@@ -96,14 +90,15 @@ namespace Grabacr07.KanColleWrapper
 
 		internal void Update(kcsapi_slotitem[] source)
 		{
-			this.droppedItemsCount = 0;
-			this.SlotItems = new MemberTable<SlotItem>(source.Select(x => new SlotItem(x)));
-		}
+            if(this.SlotItems.SetValueRange(source, x => x.api_id, x => new SlotItem(x), (obj, dat) => obj.Update(dat), true))
+                RaiseSlotItemsChanged();
+        }
 
 		internal void Update(kcsapi_useitem[] source)
 		{
-			this.UseItems = new MemberTable<UseItem>(source.Select(x => new UseItem(x)));
-		}
+			if(this.UseItems.SetValueRange(source, x => x.api_id, x => new UseItem(x), (obj, dat) => obj.Update(dat), true))
+                RaisePropertyChanged(nameof(UseItems));
+        }
 
 		internal void AddFromDock(kcsapi_kdock_getship source)
 		{
@@ -187,8 +182,8 @@ namespace Grabacr07.KanColleWrapper
 
 		private void RaiseSlotItemsChanged()
 		{
-			this.RaisePropertyChanged("SlotItems");
-			this.RaisePropertyChanged("SlotItemsCount");
+			this.RaisePropertyChanged(nameof(SlotItems));
+			this.RaisePropertyChanged(nameof(SlotItemsCount));
 		}
 	}
 }

Grabacr07.KanColleWrapper/MemberTable.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Grabacr07.KanColleWrapper.Models;
 using Livet;
+using System.Collections.Specialized;
 
 namespace Grabacr07.KanColleWrapper
 {
@@ -13,26 +14,60 @@ namespace Grabacr07.KanColleWrapper
 	/// 整数値の ID をキーとして使用する、艦これユーザー データ用のテーブルを定義します。
 	/// </summary>
 	/// <typeparam name="TValue">ユーザー データの型。</typeparam>
-	public class MemberTable<TValue> : IReadOnlyDictionary<int, TValue> where TValue : class, IIdentifiable
+	public class MemberTable<TValue> : IReadOnlyDictionary<int, TValue>, INotifyCollectionChanged where TValue : class, IIdentifiable
 	{
 		private readonly IDictionary<int, TValue> dictionary;
 
-		/// <summary>
-		/// テーブルから指定した ID の要素を取得します。ID が存在しない場合は null を返します。
-		/// </summary>
-		public TValue this[int key]
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+        
+        /// <summary>
+        /// テーブルから指定した ID の要素を取得します。ID が存在しない場合は null を返します。
+        /// </summary>
+        public TValue this[int key]
 		{
 			get { return this.dictionary.ContainsKey(key) ? this.dictionary[key] : null; }
 		}
 
 
-		public MemberTable() : this(new List<TValue>()) { }
+		public MemberTable() : this(null) { }
 
 		public MemberTable(IEnumerable<TValue> source)
 		{
-			this.dictionary = source.ToDictionary(x => x.Id);
+			this.dictionary = source?.ToDictionary(x => x.Id) ?? new Dictionary<int, TValue>();
 		}
 
+        internal bool SetValueRange<TIn>(IEnumerable<TIn> source, Func<TIn, int> idSelector, Func<TIn, TValue> valueFactory, Action<TValue, TIn> updateMethod = null, bool doDispose = false, Action<TValue> disposeMethod = null)
+        {
+            var collectionReset = false;
+            HashSet<int> removeIds = new HashSet<int>(dictionary.Keys);
+            foreach (var value in source) {
+                var id = idSelector(value);
+                removeIds.Remove(id);
+                if (updateMethod != null && dictionary.ContainsKey(id)) {
+                    updateMethod(dictionary[id], value);
+                } else {
+                    var val = valueFactory(value);
+                    if (val.Id != id) {
+                        throw new InvalidOperationException();
+                    }
+                    dictionary[id] = val;
+                    collectionReset = true;
+                }
+            }
+
+            if (doDispose) {
+                foreach (var id in removeIds) {
+                    disposeMethod?.Invoke(dictionary[id]);
+                    dictionary.Remove(id);
+                    collectionReset = true;
+                }
+            }
+            if (collectionReset) {
+                RaiseCollectionReset();
+            }
+
+            return collectionReset;
+        }
 
 		internal void Add(TValue value)
 		{
@@ -87,6 +122,12 @@ namespace Grabacr07.KanColleWrapper
 			get { return this.dictionary.Values; }
 		}
 
-		#endregion
+        #endregion
+
+        private static NotifyCollectionChangedEventArgs _resetEvent = new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset);
+        private void RaiseCollectionReset()
+        {
+            System.Windows.Application.Current.Dispatcher.BeginInvoke(new Action(() => CollectionChanged?.Invoke(this, _resetEvent)));
+        }
 	}
 }

c933103/KanColleViewer-XP

Grabacr07.KanColleWrapper/Dockyard.cs
@@ -70,20 +70,9 @@ namespace Grabacr07.KanColleWrapper
 
 		internal void Update(kcsapi_kdock[] source)
 		{
-			if (this.Docks.Count == source.Length)
-			{
-				foreach (var raw in source)
-				{
-					var target = this.Docks[raw.api_id];
-					if (target != null) target.Update(raw);
-				}
-			}
-			else
-			{
-				this.Docks.ForEach(x => x.Value.Dispose());
-				this.Docks = new MemberTable<BuildingDock>(source.Select(x => new BuildingDock(x)));
-			}
-		}
+            if(this.Docks.UpdateValueRange(source, x => x.api_id, x => new BuildingDock(x), (obj, dat) => obj.Update(dat), true, x => x.Dispose()))
+                RaisePropertyChanged(nameof(Docks));
+        }
 
 		private void GetShip(kcsapi_kdock_getship source)
 		{

Grabacr07.KanColleWrapper/Itemyard.cs
@@ -14,17 +14,11 @@ namespace Grabacr07.KanColleWrapper
 	public class Itemyard : NotificationObject
 	{
 		/// <summary>
-		/// 出撃中にドロップで入手した装備の数。
-		/// 帰投して slot_item を取得するまでは新しい装備の ID が判らないので、数だけ控えておき、SlotItemsCount で使用する。
-		/// </summary>
-		private int droppedItemsCount;
-
-		/// <summary>
 		/// <see cref="SlotItems"/> と、出撃中に入手したものを含んだ装備数を取得します。
 		/// </summary>
 		public int SlotItemsCount
 		{
-			get { return this.SlotItems.Count + droppedItemsCount; }
+			get { return this.SlotItems.Count; }
 		}
 
 		#region SlotItems 変更通知プロパティ
@@ -96,14 +90,15 @@ namespace Grabacr07.KanColleWrapper
 
 		internal void Update(kcsapi_slotitem[] source)
 		{
-			this.droppedItemsCount = 0;
-			this.SlotItems = new MemberTable<SlotItem>(source.Select(x => new SlotItem(x)));
-		}
+            if(this.SlotItems.UpdateValueRange(source, x => x.api_id, x => new SlotItem(x), (obj, dat) => obj.Update(dat), true))
+                RaiseSlotItemsChanged();
+        }
 
 		internal void Update(kcsapi_useitem[] source)
 		{
-			this.UseItems = new MemberTable<UseItem>(source.Select(x => new UseItem(x)));
-		}
+			if(this.UseItems.UpdateValueRange(source, x => x.api_id, x => new UseItem(x), (obj, dat) => obj.Update(dat), true))
+                RaisePropertyChanged(nameof(UseItems));
+        }
 
 		internal void AddFromDock(kcsapi_kdock_getship source)
 		{
@@ -116,7 +111,7 @@ namespace Grabacr07.KanColleWrapper
 
 		internal void RemoveFromShip(Ship ship)
 		{
-			foreach (var x in ship.EquippedSlots.ToArray())
+			foreach (var x in ship.EquippedSlots)
 			{
 				this.SlotItems.Remove(x.Item);
 			}
@@ -187,8 +182,8 @@ namespace Grabacr07.KanColleWrapper
 
 		private void RaiseSlotItemsChanged()
 		{
-			this.RaisePropertyChanged("SlotItems");
-			this.RaisePropertyChanged("SlotItemsCount");
+			this.RaisePropertyChanged(nameof(SlotItems));
+			this.RaisePropertyChanged(nameof(SlotItemsCount));
 		}
 	}
 }

Grabacr07.KanColleWrapper/MemberTable.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Grabacr07.KanColleWrapper.Models;
 using Livet;
+using System.Collections.Specialized;
 
 namespace Grabacr07.KanColleWrapper
 {
@@ -13,26 +14,60 @@ namespace Grabacr07.KanColleWrapper
 	/// 整数値の ID をキーとして使用する、艦これユーザー データ用のテーブルを定義します。
 	/// </summary>
 	/// <typeparam name="TValue">ユーザー データの型。</typeparam>
-	public class MemberTable<TValue> : IReadOnlyDictionary<int, TValue> where TValue : class, IIdentifiable
+	public class MemberTable<TValue> : IReadOnlyDictionary<int, TValue>, INotifyCollectionChanged where TValue : class, IIdentifiable
 	{
 		private readonly IDictionary<int, TValue> dictionary;
 
-		/// <summary>
-		/// テーブルから指定した ID の要素を取得します。ID が存在しない場合は null を返します。
-		/// </summary>
-		public TValue this[int key]
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+        
+        /// <summary>
+        /// テーブルから指定した ID の要素を取得します。ID が存在しない場合は null を返します。
+        /// </summary>
+        public TValue this[int key]
 		{
 			get { return this.dictionary.ContainsKey(key) ? this.dictionary[key] : null; }
 		}
 
 
-		public MemberTable() : this(new List<TValue>()) { }
+		public MemberTable() : this(null) { }
 
 		public MemberTable(IEnumerable<TValue> source)
 		{
-			this.dictionary = source.ToDictionary(x => x.Id);
+			this.dictionary = source?.ToDictionary(x => x.Id) ?? new Dictionary<int, TValue>();
 		}
 
+        internal bool UpdateValueRange<TIn>(IEnumerable<TIn> source, Func<TIn, int> idSelector, Func<TIn, TValue> valueFactory, Action<TValue, TIn> updateMethod = null, bool doDispose = false, Action<TValue> disposeMethod = null)
+        {
+            var collectionReset = false;
+            HashSet<int> removeIds = new HashSet<int>(dictionary.Keys);
+            foreach (var value in source) {
+                var id = idSelector(value);
+                removeIds.Remove(id);
+                if (updateMethod != null && dictionary.ContainsKey(id)) {
+                    updateMethod(dictionary[id], value);
+                } else {
+                    var val = valueFactory(value);
+                    if (val.Id != id) {
+                        throw new InvalidOperationException();
+                    }
+                    dictionary[id] = val;
+                    collectionReset = true;
+                }
+            }
+
+            if (doDispose) {
+                foreach (var id in removeIds) {
+                    disposeMethod?.Invoke(dictionary[id]);
+                    dictionary.Remove(id);
+                    collectionReset = true;
+                }
+            }
+            if (collectionReset) {
+                RaiseCollectionReset();
+            }
+
+            return collectionReset;
+        }
 
 		internal void Add(TValue value)
 		{
@@ -87,6 +122,12 @@ namespace Grabacr07.KanColleWrapper
 			get { return this.dictionary.Values; }
 		}
 
-		#endregion
+        #endregion
+
+        private static NotifyCollectionChangedEventArgs _resetEvent = new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset);
+        private void RaiseCollectionReset()
+        {
+            System.Windows.Application.Current.Dispatcher.BeginInvoke(new Action(() => CollectionChanged?.Invoke(this, _resetEvent)));
+        }
 	}
 }