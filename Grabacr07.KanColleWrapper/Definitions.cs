751ba621dc89cf41909ae9be6ae8521450488c05
Linnaea Von Lavia <linnaea-von-lavia@live.com>
2015-07-26 11:57:31 +0800

    +update check
    LynLogger: color-code TextBox to indicate its mode.

---
+++ Grabacr07.KanColleWrapper/Definitions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grabacr07.KanColleWrapper
+{
+	public static class Definitions
+	{
+		public static readonly DateTimeOffset UnixEpoch = new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero);
+
+        public static long UnixMillisecondTimestamp => (long)(DateTimeOffset.UtcNow - UnixEpoch).TotalMilliseconds;
+        public static long UnixTimestamp => (long)(DateTimeOffset.UtcNow - UnixEpoch).TotalSeconds;
+    }
+}
